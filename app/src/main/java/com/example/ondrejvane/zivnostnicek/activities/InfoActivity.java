b93e67364c4569c736f4b28088a1aee90971487c
ondrejvane <onry.vane@gmail.com>
2018-12-15 22:24:14 +0100

    Implemented GUI for info activity.

--- app/src/main/java/com/example/ondrejvane/zivnostnicek/activities/InfoActivity.java
+++ app/src/main/java/com/example/ondrejvane/zivnostnicek/activities/InfoActivity.java
@@ -1,6 +1,7 @@
 package com.example.ondrejvane.zivnostnicek.activities;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,18 +25,13 @@ public class InfoActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_info);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
