41c41441f7c1514a5cbc1f2ecb9728d2bd460192
ondrejvane <onry.vane@gmail.com>
2019-03-10 14:01:19 +0100

    Trader address show in map.

--- app/src/main/java/com/example/ondrejvane/zivnostnicek/activities/trader/TraderShowFragment.java
+++ app/src/main/java/com/example/ondrejvane/zivnostnicek/activities/trader/TraderShowFragment.java
@@ -1,12 +1,16 @@
 package com.example.ondrejvane.zivnostnicek.activities.trader;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ondrejvane.zivnostnicek.R;
 import com.example.ondrejvane.zivnostnicek.database.TraderDatabaseHelper;
@@ -21,13 +25,12 @@ public class TraderShowFragment extends Fragment {
     private EditText inputCompanyNameShow, inputContactPersonShow, inputTelephoneNumberShow;
     private EditText inputIdentificationNumberShow, inputTaxIdentificationNumberShow;
     private EditText inputCityShow, inputStreetShow, inputHouseNumberShow;
+    private Button buttonShowMap;
 
     //pomocné globální proměnné
     private TraderDatabaseHelper traderDatabaseHelper;
     private int traderID;
     private Trader trader;
-
-
 
 
     public TraderShowFragment() {
@@ -45,6 +48,14 @@ public class TraderShowFragment extends Fragment {
 
         //zobrazení informací o bochodníkovi do fragmentu
         setTextToFragment();
+
+        //nastavení listeneru pro zobrazení adresy obchodníka v mapě
+        buttonShowMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMap();
+            }
+        });
 
         // Inflate the layout for this fragment
         return view;
@@ -74,6 +85,23 @@ public class TraderShowFragment extends Fragment {
         inputCityShow = view.findViewById(R.id.textInputEditTextCityShow);
         inputStreetShow = view.findViewById(R.id.textInputEditTextStreetShow);
         inputHouseNumberShow = view.findViewById(R.id.textInputEditTextHouseNumberShow);
+        buttonShowMap = view.findViewById(R.id.buttonShowMap);
+    }
+
+    private void showMap() {
+        String city = inputCityShow.getText().toString();
+        String street = inputStreetShow.getText().toString();
+        String houseNumber = inputHouseNumberShow.getText().toString();
+
+        if (!city.isEmpty()) {
+            String address = street + "," + houseNumber + "," + city;
+            String map = "http://maps.google.co.in/maps?q=" + address;
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
+            startActivity(i);
+        } else {
+            Toast.makeText(getContext(), "Message is empty", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
